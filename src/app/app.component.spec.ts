c74c271387da536292083e8af3ef845d1cc8976a
JoseLu-Dev <joseludev@gmail.com>
2021-07-30 19:00:25 +0200

    Changed app name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'secure-chat-frontend'`, () => {
+  it(`should have as title 'socket.io-chat-frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('secure-chat-frontend');
+    expect(app.title).toEqual('socket.io-chat-frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('secure-chat-frontend app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('socket.io-chat-frontend app is running!');
   });
 });
